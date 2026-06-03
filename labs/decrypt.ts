98ee952bc18c2b8c679fb7dbb645c94008f5dfec
rafiarrafif <rafiarrafif05@gmail.com>
2026-06-03 21:00:00 +0700

    👔 feat: support decryption of strings longer than 3 characters

--- labs/decrypt.ts
+++ labs/decrypt.ts
@@ -55,25 +55,34 @@ export const decrypt = () => {
   if (gcd(determinantKey, 26) !== 1)
     return console.log("Key tidak valid, tidak memiliki invers modulo 26");
 
-  const cipherText = "ZNH";
+  const cipherText = "FQKIVQKOMCUE";
   const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const cipherTextInNumber = cipherText
     .split("")
     .map((c) => char.indexOf(c))
     .filter((num) => num !== -1);
-  const [c1, c2, c3] = cipherTextInNumber;
 
-  const p1 = mod(
-    c1 * keyInverse[0][0] + c2 * keyInverse[0][1] + c3 * keyInverse[0][2],
-  );
-  const p2 = mod(
-    c1 * keyInverse[1][0] + c2 * keyInverse[1][1] + c3 * keyInverse[1][2],
-  );
-  const p3 = mod(
-    c1 * keyInverse[2][0] + c2 * keyInverse[2][1] + c3 * keyInverse[2][2],
-  );
-  const decryptedString = char[p1] + char[p2] + char[p3];
-  console.log(`Decrypted String: ${JSON.stringify(decryptedString)}`);
+  const pAll = [];
+  for (let iterate = 0; iterate < cipherTextInNumber.length; iterate += 3) {
+    const c1 = cipherTextInNumber[iterate];
+    const c2 = cipherTextInNumber[iterate + 1];
+    const c3 = cipherTextInNumber[iterate + 2];
+
+    const p1 = mod(
+      c1 * keyInverse[0][0] + c2 * keyInverse[0][1] + c3 * keyInverse[0][2],
+    );
+    const p2 = mod(
+      c1 * keyInverse[1][0] + c2 * keyInverse[1][1] + c3 * keyInverse[1][2],
+    );
+    const p3 = mod(
+      c1 * keyInverse[2][0] + c2 * keyInverse[2][1] + c3 * keyInverse[2][2],
+    );
+    pAll.push(p1, p2, p3);
+  }
+
+  const decryptedString = pAll.map((num) => char[num]).join("");
+  console.log("Decrypted String:", decryptedString);
+  return decryptedString;
 };
 
 decrypt();
